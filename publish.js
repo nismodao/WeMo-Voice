0446ea079c336a29e16dbd5e210223fe10c5e6ad
Nam Dao <nam.d.dao@gmail.com>
2016-07-03 00:33:26 -0700

    updated client

--- publish.js
+++ publish.js
@@ -1,6 +1,5 @@
-var AWS = require('aws-sdk');
 require('dotenv').config();
-
+var AWS = require('aws-sdk');
 AWS.config.update({
   region: 'us-west-1'
 });
@@ -8,17 +7,16 @@ var sqs = new AWS.SQS();
 
 module.exports = {
   send: (req, res) => {
-    var message = req.body.message.match(/turn on | turn off/);
+    var message = req.body.message.match(/turn on|turn off/i);
     (!!message) ? message = message[0].trim() : message = 'not a valid input';
     var params = {
       MessageBody: message + '',
-      QueueUrl: 'https://sqs.us-west-1.amazonaws.com/996941631084/Wemo',
-      DelaySeconds: 0
+      QueueUrl: '',
+      DelaySeconds: 0,
+      QueueUrl: process.env.queue_URL
     };
-    console.log('params is', params);
     sqs.sendMessage(params, function(err, data) {
-      if (err) console.log(err, err.stack); 
-      else     console.log(data);        
+      (err) ? console.log(err, err.stack) : console.log(data);        
     });
     res.end();
   }
